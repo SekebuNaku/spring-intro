b3dc4307b05cf491e69d55cb2721858b2595fe17
Sekebu Naku <sekebunaku@gmail.com>
2020-12-17 13:36:57 +0100

    zadanie 2

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,23 +1,21 @@
 package com.example;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MyApp {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(myConfig.class);
 
-        MessageService messageServiceA = applicationContext.getBean("messageService", MessageService.class);
-        MessageService messageServiceB = applicationContext.getBean("messageService", MessageService.class);
+        MessageService messageServiceA = applicationContext.getBean("myNameMessageService", MessageService.class);
+        MessageService messageServiceB = applicationContext.getBean("myNameMessageService", MessageService.class);
+        MessageService messageServiceC = applicationContext.getBean("randomTextMessageService", MessageService.class);
 
-        System.out.println("First messange: " + messageServiceA.hashCode());
-        System.out.println("Second messange: " + messageServiceB.hashCode());
-
-        MessageService messageServiceC = applicationContext.getBean("randomMessageService", MessageService.class);
+        System.out.println("Hashcodes comparition: " + messageServiceA.hashCode()+" "+messageServiceB.hashCode());
 
         System.out.println(messageServiceA.getMessage());
         System.out.println(messageServiceC.getMessage());
 
-        applicationContext.close();
     }
 }
